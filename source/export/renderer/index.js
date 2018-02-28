ede477aa5709d6a763c4f5716a7e8fd9d6c0e004
Artur Baron <P224435@tk.de>
2018-02-28 13:28:23 +0100

    added a renderer for IfNodes

--- source/export/renderer/index.js
+++ source/export/renderer/index.js
@@ -4,9 +4,11 @@
 module.exports =
 {
     FtlVariableNodeRenderer: require('./FtlVariableNodeRenderer.js').FtlVariableNodeRenderer,
+    FtlIfNodeRenderer: require('./FtlIfNodeRenderer.js').FtlIfNodeRenderer,
 
     rendererList:
     [
+        require('./FtlIfNodeRenderer.js').FtlIfNodeRenderer,
         require('./FtlVariableNodeRenderer.js').FtlVariableNodeRenderer,
         require('entoj-system').export.renderer.TextNodeRenderer,
         require('entoj-system').export.renderer.NodeListRenderer
