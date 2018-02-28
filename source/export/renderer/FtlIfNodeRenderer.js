0a4ad75888b269019b7d0391ea1897cc370b78f3
Artur Baron <P224435@tk.de>
2018-02-28 12:13:41 +0100

    added a renderer for IfNodes

--- source/export/renderer/FtlIfNodeRenderer.js
+++ source/export/renderer/FtlIfNodeRenderer.js
@@ -5,7 +5,7 @@
  * @ignore
  */
 const NodeRenderer = require('entoj-system').export.renderer.NodeRenderer;
-
+const co = require('co');
 
 /**
  * Renders a if .. else ..  as FTL
@@ -39,8 +39,56 @@ class FtlIfNodeRenderer extends NodeRenderer
         {
             return Promise.resolve('');
         }
-        let result = 'IF';
-        return Promise.resolve(result);
+        const promise = co(function*()
+        {
+            let result = '';
+
+            // ... ? ... : ....
+            if (node.parent && node.parent.is('ExpressionNode'))
+            {
+                result+= yield configuration.renderer.renderNode(node.condition, configuration);
+                result+= '?then(';
+                result+= yield configuration.renderer.renderList(node.children, configuration);
+                result+= ', ';
+                result+= yield configuration.renderer.renderList(node.elseChildren, configuration);
+                result+= ')';
+            }
+            // If ...
+            else if (!node.elseChildren.length && !node.elseIfChildren.length)
+            {
+                result+= '<#if ';
+                result+= yield configuration.renderer.renderNode(node.condition, configuration);
+                result+= '>';
+                result+= yield configuration.renderer.renderList(node.children, configuration);
+                result+= '</#if>';
+            }
+            // If ... elseif ... else ...
+            else
+            {
+                result+= '<#if ';
+                result+= yield configuration.renderer.renderNode(node.condition, configuration);
+                result+= '>';
+                result+= yield configuration.renderer.renderList(node.children, configuration);
+                if (node.elseIfChildren.length)
+                {
+                    for (const elseIfNode of node.elseIfChildren)
+                    {
+                        result+= '<#elseif ';
+                        result+= yield configuration.renderer.renderNode(elseIfNode.condition, configuration);
+                        result+= '>';
+                        result+= yield configuration.renderer.renderList(elseIfNode.children, configuration);
+                    }
+                }
+                if (node.elseChildren.length)
+                {
+                    result+= '<#else>';
+                    result+= yield configuration.renderer.renderList(node.elseChildren, configuration);
+                }
+                result+= '</#if>';
+            }
+            return result;
+        });
+        return promise;
     }
 }
 
--- test/__fixtures__/nodeRenderer/FtlIfNodeRenderer.expected.ftl
+++ test/__fixtures__/nodeRenderer/FtlIfNodeRenderer.expected.ftl
@@ -1,1 +1,15 @@
-FAIL
+<#if >COPY</#if>
+
+<#if >COPY<#else>NOCOPY</#if>
+
+<#if >
+I am hungry
+<#elseif >
+I am tired
+<#elseif >
+I am thirsty
+<#else>
+I am good!
+</#if>
+
+?then(, )
